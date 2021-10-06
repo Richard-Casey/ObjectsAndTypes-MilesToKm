689a61ecff05d54472912f32b5d88a489d5d099d
Richard Casey <91665484+Richard-Casey@users.noreply.github.com>
2021-10-06 20:48:40 +0100

    Add project files.

---
+++ Project106102021/Source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+
+int main()
+{
+	std::cout << "Hi! Please enter the distance you have travelled in miles so it can be converted into kilometers. This is done so that it appears that you have travelled further than you actually have when posting on social media.#WitnesstheFitness #DadBodsRuleOK #Hereditary";
+	double q;
+	std::cin >> q;
+	std::cout << "Not bad, " << q << " miles is equal to " << q * 1.609344 <<" Kilometers! You go you little trooper you.";
+}
